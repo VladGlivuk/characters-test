ce52b587b995cb68c71806fa61c9b51a4b1c1b16
Vlad Glivuk <vlad.glivuk@gmail.com>
2024-11-27 11:34:31 +0200

    Configure API

---
+++ src/services/api/requests.ts
@@ -0,0 +1,20 @@
+import axiosInstance from "./axiosInstance";
+import {
+  HttpCharactersParams,
+  HttpPaginatedCharacters,
+  HttpCharacter,
+} from "./types";
+
+export async function httpGetCharacters(params: HttpCharactersParams) {
+  const response = await axiosInstance.get<HttpPaginatedCharacters>("/people", {
+    params,
+  });
+
+  return response.data;
+}
+
+export async function httpGetCharacterById(id: number) {
+  const response = await axiosInstance.get<HttpCharacter>(`/people/${id}`);
+
+  return response.data;
+}
---
+++ src/services/api/types.ts
@@ -0,0 +1,33 @@
+export type HttpCharactersParams = {
+  name?: string;
+  page?: number;
+};
+
+export type HttpCharacter = {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+  films: Array<string>;
+  species: Array<string>;
+  starships: Array<string>;
+  vehicles: Array<string>;
+  url: string;
+  created: string; // ISO 8601 date
+  edited: string; // ISO 8601 date
+};
+
+export type HttpPagination = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+}
+
+export type HttpPaginatedCharacters = HttpPagination & {
+  results: Array<HttpCharacter>;
+};
